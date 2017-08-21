affba79838ba23143edb2dcf963eb5db60255a76
losephjambert <josephc.lambert@gmail.com>
2017-08-21 01:28:53 -0700

    all the things

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `0kswtu653u0m`,
+        accessToken: `f05698ad5ef43d41df517331205acf50ba500938b5dd6ca1354b3fb201713b76`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+     'gatsby-plugin-catch-links',
+  ],
+}
---
+++ src/pages/page-2.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const FoodItem = ({ node }) =>
+  <div key={node.id}>
+    <div>{node.name}</div>
+    <div>${node.price}</div>
+    <div>{ node.ingredients.map(( ingredient, i) => <li key={i}>{ ingredient }</li> )}</div>
+    <div>{ node.labels.map(( label, i) => <div key={i}>{ label.label }</div> )}</div>
+  </div>
+
+const DrinkItem = ({ node }) =>
+  <div key={node.id}>
+    <div>{node.name}</div>
+    <div>${node.price}</div>
+    <div>
+    { node.sizes.map(( size, i) => 
+      size !== 'no size' &&
+      <div key={i}>{ size }</div>
+    )}
+    </div>
+  </div>
+
+class SecondPage extends React.Component {
+  render() {
+    const foodItem = this.props.data.allContentfulFood.edges
+    const drinkItem = this.props.data.allContentfulDrink.edges
+    console.log(drinkItem)
+    return (
+      <div>
+        <div>Food</div>
+        <div>
+          { foodItem.map(( {node}, i) => <FoodItem key={i} node={node} /> )}
+        </div>
+        <div>Drink</div>
+        <div>~~~~~~~~~~~~~~~~~~~~~</div>
+        <div>
+          { drinkItem.map(( {node}, i) => <DrinkItem key={i} node={node} /> )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SecondPage
+
+export const pageQuery = graphql`
+  query PageQuery {
+    allContentfulFood{
+      edges {
+        node{
+          id
+          name
+          price
+          ingredients
+          labels{
+            label
+          }
+        }
+      }
+    }
+    allContentfulDrink{
+      edges {
+        node{
+          id
+          name
+          price
+          sizes
+        }
+      }
+    }
+  }
+`
