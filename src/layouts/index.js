3564a2a2e5ff065d509820281d4a51eb1a45714d
losephjambert <josephc.lambert@gmail.com>
2017-09-01 11:27:46 -0700

    quick splash build

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,32 +6,7 @@ import Helmet from 'react-helmet'
 import './index.css'
 
 const Header = () =>
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <div/>
 
 const TemplateWrapper = ({ children }) =>
   <div>
@@ -43,14 +18,7 @@ const TemplateWrapper = ({ children }) =>
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,16 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () =>
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+  <div className="container">
+    <div className="border border-top"></div>
+    <div className="border border-bottom"></div>
+    <div className="standard-background-image top-left-animation"></div>
+    <div className="standard-background-image bottom-right-animation"></div>
+    <div className="standard-background-image logo"></div>
+    {/* 
+    Uncomment to show link to page 2
     <Link to="/page-2/">Go to page 2</Link>
+    */ } 
   </div>
 
 export default IndexPage
