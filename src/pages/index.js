22c886ae8c07be9c87ef653b92fb1154a4819ffa
losephjambert <josephc.lambert@gmail.com>
2017-09-01 13:34:44 -0700

    solves assets, maybe

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Logo from '../assets/logo-full.png'
+import BorderTop from '../assets/border.svg'
+import BorderBottom from '../assets/border-bottom.svg'
+import Patio from '../assets/patio.svg'
+import Days from '../assets/days.svg'
 
 const IndexPage = () =>
   <div className="container">
-    <div className="border border-top"></div>
-    <div className="border border-bottom"></div>
-    <div className="standard-background-image top-left-animation"></div>
-    <div className="standard-background-image bottom-right-animation"></div>
-    <div className="standard-background-image logo"></div>
+    <div 
+      style={{backgroundImage: `url(${BorderTop})`}}
+      className="border border-top"></div>
+    <div 
+      style={{backgroundImage: `url(${BorderBottom})`}}
+      className="border border-bottom"></div>
+    <div 
+      style={{backgroundImage: `url(${Days})`}}
+      className="standard-background-image top-left-animation"></div>
+    <div 
+      style={{backgroundImage: `url(${Patio})`}}
+      className="standard-background-image bottom-right-animation"></div>
+    <div 
+      style={{backgroundImage: `url(${Logo})`}}
+      className="standard-background-image logo"></div>
     {/* 
     Uncomment to show link to page 2
     <Link to="/page-2/">Go to page 2</Link>
